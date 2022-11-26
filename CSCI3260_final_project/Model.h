4665df982984ae747751bb35a93d297127e29efa
cheungkitleong <cheungkitleong@gmail.com>
2022-11-26 23:15:55 +0800

    update

--- CSCI3260_final_project/Model.h
+++ CSCI3260_final_project/Model.h
@@ -1,6 +1,32 @@
 #pragma once
+#include "Dependencies/glm/glm.hpp"
 
-class Model {
+#include <vector>
+//#include <string>
+//#include <iostream>
+//#include <fstream>
 
+
+struct Vertex {
+	glm::vec3 position;
+	glm::vec2 uv;
+	glm::vec3 normal;
+};
+
+class Model 
+{
+public:
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+
+	Model(const char* path)
+	{
+		loadOBJ(path);
+	}
+
+private:
+
+	// load model from path
+	void loadOBJ(const char* objPath);
 
 };
